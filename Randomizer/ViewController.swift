ae633fe5fbd60be88bf906c83e94a53b88894108
Will Oakley <woakley5@icloud.com>
2018-12-07 14:08:21 -0800

    Initial Commit

---
+++ Randomizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Randomizer
+//
+//  Created by Will Oakley on 12/7/18.
+//  Copyright © 2018 Mobile Developers of Berkeley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
